7f5bab45677c78818309621c022757a661c9741e
“JG” <jesus@guzman1.com>
2023-05-27 23:05:59 -0700

    Init with stow cmd

---
+++ config/.config/scripts/appsInstallation.sh
@@ -0,0 +1,238 @@
+#!/usr/bin/env bash
+
+# Homebrew Script for OSX
+# To execute: save and `chmod +x ./*.sh` then `./*.sh`
+#
+echo "------------------------------------"
+echo "Adjusting macos system settings..."
+echo "------------------------------------"
+defaults write com.apple.finder AppleShowAllFiles YES
+defaults write com.apple.finder CreateDesktop false
+killall Finder
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+defaults write NSGlobalDomain AppleScrollerPagingBehavior -int 1
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.Finder FXPreferredViewStyle clmv
+defaults write com.apple.dock magnification -bool false
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.Safari ShowFavoritesBar -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari ShowOverlayStatusBar -bool true
+defaults write com.apple.dock static-only -bool TRUE 
+killall Dock
+echo "------------------------------------"
+echo "Done adjusting"
+echo "------------------------------------"
+
+
+# Check for Homebrew and install if we don't have it
+if ! which brew>/dev/null; then
+  echo "Installing homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+installHomeBrewApps() {
+  # Dev Tools
+  echo "------------------------------------"
+  echo "Installing development tools..."
+  echo "------------------------------------"
+  brew tap homebrew/cask
+  brew install stow
+  brew install --cask docker
+  brew install git
+  brew install gh
+  brew install --cask github
+  brew install --cask iterm2
+  brew install --cask visual-studio-code
+  brew install neovim
+
+  # Web Tools
+  echo "------------------------------------"
+  echo "Installing web tools..."
+  echo "------------------------------------"
+  brew install --cask microsoft-edge
+  brew install --cask google-chrome
+  brew install httpie
+  brew install http-server
+  brew install node
+  brew install fnm
+  brew install --cask ngrok
+  brew install pnpm
+  brew install --cask postman
+  brew install nginx
+
+  # Communication Apps
+  echo "------------------------------------"
+  echo "Installing communication apps..."
+  echo "------------------------------------"
+  brew install --cask slack
+  brew install --cask zoom 
+
+  # Productivity Apps
+  echo "------------------------------------"
+  echo "Installing productivity apps..."
+  echo "------------------------------------"
+  brew install --cask hazeover
+  brew install --cask raycast
+  brew install --cask akiflow
+  brew install --cask figma
+  brew install --cask shottr
+  brew install --cask gifox
+  brew install --cask obsidian
+  brew install --cask obs
+  brew install --cask appcleaner
+  brew install --cask sizzy
+  brew install koekeishiya/formulae/yabai
+  brew install koekeishiya/formulae/skhd
+  brew install tmux
+  brew install jesseduffield/lazygit/lazygit
+  brew install jesseduffield/lazynpm/lazynpm
+  brew install starship
+
+  # DevOps Tools
+  echo "------------------------------------"
+  echo "Installing devops tools..."
+  echo "------------------------------------"
+  brew install --cask aws-vault
+  brew install awscli
+  brew install terraform
+  brew install kubectl
+  brew install kubectx
+  brew install kubens
+  brew install k9s
+  brew install helm
+
+  # Database Tools
+  echo "------------------------------------"
+  echo "Installing database tools..."
+  echo "------------------------------------"
+  brew install --cask tableplus
+  brew install --cask robo-3t
+
+  # Fonts
+  echo "------------------------------------"
+  echo "Installing fonts..."
+  echo "------------------------------------"
+  brew tap homebrew/cask-fonts
+  brew install --cask font-hack-nerd-font
+  brew install --cask font-jetbrains-mono-nerd-font
+
+  echo "********** DONE **********"
+
+  return 0
+}
+
+configureGit() {
+  echo "------------------------------------"
+  echo "Configuring git..."
+  echo "------------------------------------"
+  # git config --global user.name "JG"
+  # git config --global user.email "jesus@guzman1.com"
+  git config --global push.autoSetupRemote true
+
+      echo "WARNING: not checking for existing SSH keys\!"
+
+    # Use current branch only when doing git push
+    git config --global push.default current
+
+    echo "What’s your git name?"
+    read GIT_SETUP_NAME
+    git config --global user.name $GIT_SETUP_NAME
+
+    echo "What’s your git email?"
+    read GIT_SETUP_EMAIL
+    git config --global user.email $GIT_SETUP_EMAIL
+
+    echo "Now configuring SSH keys..."
+    ssh-keygen -t rsa -C $GIT_SETUP_EMAIL
+
+    echo "Let’s start the ssh-agent..."
+    eval "$(ssh-agent -s)"
+
+    echo "Adding SSH key..."
+    ssh-add ~/.ssh/id_rsa
+
+    echo "Now copying SSH key to clipboard..."
+    pbcopy < ~/.ssh/id_rsa.pub
+
+    # Use the patience algorithm for diffing
+    git config --global diff.algorithm patience
+
+    git config --global diff.colorMoved default
+
+  echo "********** DONE **********"
+
+  return 0
+}
+
+openAppsFromSite() {
+  echo "------------------------------------"
+  echo "Opening apps from website..."
+  echo "------------------------------------"
+  open -a "Microsoft Edge" https://www.dev-box.app/ https://www.corsair.com/us/en/downloads https://www.logitech.com/en-us/video-collaboration/software/logi-tune-software.html https://www.lunarvim.org/docs/installation https://www.zapzsh.org/ https://github.com/jimeh/tmuxifier
+
+  echo "********** DONE **********"
+
+  return 0
+}
+
+createRepoDir() {
+  echo "-----------------"
+  echo "Creating repos directory ~/repos"
+  echo "-----------------"
+  mkdir ~/repos
+
+  echo "********** DONE **********"
+
+  return 0
+}
+
+copyConfigFiles() {
+  echo "-----------------"
+  echo "Coping files from ~/.dotfiles to ~/.config"
+  echo "-----------------"
+
+  cp -R ~/.dotfiles/github-copilot ~/.dotfiles/iterm2 ~/.dotfiles/lvim ~/.dotfiles/raycast ~/.dotfiles/skhd ~/.dotfiles/starship.toml ~/.dotfiles/tmux ~/.dotfiles/yabai ~/.config/
+
+  echo "-----------------"
+  echo "Coping zsrc from ~/.dotfiles to ~/.zshrc"
+  echo "-----------------"
+
+  cp -R ~/.dotfiles/.zshrc ~/.zshrc
+
+  echo "-----------------"
+  echo "Sourcing tmux & zshrc"
+  echo "-----------------"
+
+  source ~/.config/tmux/tmux.conf
+  source ~/.zshrc
+
+  return 0
+
+}
+
+if which brew>/dev/null; then
+  # Installing Homebrew Apps
+  echo "------------------------------------"
+  echo "Installing Homebrew Apps..."
+  echo "------------------------------------"
+  installHomeBrewApps "$@"
+  wait
+  configureGit "$@"
+  wait
+  openAppsFromSite "$@"
+  wait
+  createRepoDir "$@"
+  wait
+  copyConfigFiles "$@"
+  echo "------------------------------------"
+  echo "Setup is complete 🎉..."
+  echo "------------------------------------"
+
+fi
---
+++ zshrc/.zshrc
@@ -0,0 +1,93 @@
+# Created by Zap installer
+# https://www.zapzsh.org/
+[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"
+plug "zsh-users/zsh-autosuggestions"
+plug "zap-zsh/supercharge"
+plug "zap-zsh/zap-prompt"
+plug "zsh-users/zsh-syntax-highlighting"
+plug "zap-zsh/exa"
+
+
+## utils aliases
+alias home='~/'
+alias update='source ~/.zshrc';
+alias update-tmux='tmux source ~/.config/tmux/tmux.conf';
+alias update-lvim='source ~/.config/lvim';
+alias l='exa --icons --group-directories-first -a'
+alias ll='exa -l --icons --no-user --group-directories-first  --time-style long-iso'
+alias la='exa -la --icons --no-user --group-directories-first  --time-style long-iso'
+alias lg='lazygit';
+alias lnpm='lazynpm';
+alias localserver='lite-server';
+alias cls='clear';
+alias repos='~/repos';
+alias rootdir='~/';
+alias delDir='rm -rf';
+
+## git aliases
+## git add/commit/push
+function gpush() {
+    git add .
+    if [ "$1" != "" ]
+    then
+        git commit -m "$1"
+    else
+        git commit -m update # default commit message is `update`
+    fi # closing statement of if-else block
+    git push origin HEAD
+}
+alias greset="git restore .";
+alias gc="git commit -m";
+alias gnb="git checkout -b"
+alias gcb-local="git checkout";
+function gcb-remote() { git switch -c "$1" origin/"$1"; }
+alias gs="git status";
+alias gp="git pull";
+alias gf="git fetch";
+alias gpush="git push";
+alias gd="git diff";
+alias ga="git add .";
+alias gac="git add . && oc";
+alias gdel-local="git branch -D";
+alias gdel-remote="git push orgin --delete";
+alias glog='git log --oneline';
+alias gfindbycommit='git log --all --grep';
+alias gfindbydeveloper='git log --author';
+function gdiff() { git diff --color --no-index "$1" "$2" | diff-so-fancy; };
+function cdiff() { code --diff "$1" "$2"; };
+
+## LunarVIM
+alias lvim="~/.local/bin/lvim"
+
+## npm aliases
+alias ni="npm install";
+alias nrs="npm run start -s --";
+alias nrb="npm run build -s --";
+alias nrd="npm run dev -s --";
+alias nrt="npm run test -s --";
+alias nrtw="npm run test:watch -s --";
+alias nrv="npm run validate -s --";
+alias rmn="rm -rf node_modules";
+alias npm-clean="rm -rf node_modules package-lock.json && npm cache clean --force && npm i --force && say NPM is done";
+alias nicache="npm install --prefer-offline";
+alias nioff="npm install --offline";
+alias db-push="npm run db-push";
+
+# ~/.tmux/plugins
+export PATH=$HOME/.tmux/plugins/t-smart-tmux-session-manager/bin:$PATH
+# ~/.config/tmux/plugins
+export PATH=$HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin:$PATH
+
+## Fire UP Starship https://starship.rs/
+eval "$(starship init zsh)"
+
+## Fire UP FNM
+eval "$(fnm env --use-on-cd)"
+
+# pnpm
+export PNPM_HOME="/Users/jesus/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
