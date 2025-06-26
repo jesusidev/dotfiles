b0eb2fe7d1b7bb95ee1d2d5de7882cec965d4222
jesusidev <jesus@guzman1.com>
2025-06-26 13:39:50 -0700

    Updated lua

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,43 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
 --
-local function augroup(name)
-  return vim.api.nvim_create_augroup("my_lazyvim_" .. name, { clear = true })
-end
-
--- Set filetype for .env and .env.* files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = augroup("env_filetype"),
-  pattern = { "*.env", ".env.*" },
-  callback = function()
-    vim.opt_local.filetype = "sh"
-  end,
-})
-
--- Set filetype for .hurl files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = augroup("hurl_filetype"),
-  pattern = { "*.hurl" },
-  callback = function()
-    vim.opt_local.filetype = "hurl"
-  end,
-})
-
--- Set filetype for .toml files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = augroup("toml_filetype"),
-  pattern = { "*.tomg-config*" },
-  callback = function()
-    vim.opt_local.filetype = "toml"
-  end,
-})
-
--- Set filetype for .ejs files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = augroup("ejs_filetype"),
-  pattern = { "*.ejs", "*.ejs.t" },
-  callback = function()
-    vim.opt_local.filetype = "embedded_template"
-  end,
-})
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- nvim/.config/nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "rcarriga/nvim-notify", enabled = false },
-  -- Disabled Buffer tabs
-  {"bufferline.nvim", enabled = false },
-  {"m4xshen/hardtime.nvim", enabled = false},
-}
