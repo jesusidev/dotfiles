7c5038d0c808099a3f83a34156ce36484bd2fbd1
jesusidev <jesus@guzman1.com>
2025-10-30 06:30:35 -0700

    feat(prisma): add LSP and formatting support

---
+++ nvim/.config/nvim/lua/plugins/prisma.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "prisma-language-server",
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        prismals = {},
+      },
+    },
+  },
+}
